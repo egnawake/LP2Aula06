023f45e45eeec6632e55acebebfc84537f7d377a
João Inácio <egnawake@gmail.com>
2022-11-08 09:33:56 +0000

    Add files to BearAdapter

--- BearAdapter/Program.cs
+++ BearAdapter/Program.cs
@@ -6,7 +6,23 @@ namespace BearAdapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Create a german shepherd and dog-adapted gray wolf
+            IDog dog1 = new GermanShepherd();
+            IDog dog2 = new WolfAdapter(new GrayWolf());
+
+            // Run dog methods on the german shepherd
+            Console.WriteLine($"== Dog 1 ({dog1.GetType().Name}) ==");
+            dog1.Bark();
+            dog1.Fetch("toy");
+
+            Console.WriteLine();
+
+            // Run dog methods on the dog-adapted gray wolf
+            Console.WriteLine($"== Dog 2 ({dog2.GetType().Name}) ==");
+            dog2.Bark();
+            dog2.Fetch("rabbit");
+
+            Console.WriteLine();
         }
     }
 }
